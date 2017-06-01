d587108709a533f6f93938597f03993a96ae2954
Josh Sun <sunjo@omers.com>
2017-06-01 10:23:59 -0400

    merging newer version

--- public/js/grid.js
+++ public/js/grid.js
@@ -2,43 +2,64 @@ var columns = [
   {
     name: "control",
     type: "control",
-    width:"100px",
+    width:"30px",
     editButton: false,
   },
-  { name: "ID", type: "text", width:"300px", align:"center"}, //hidden
-  { name: "Status", type: "select", items: [{Id: ""},{Id: "submitted"},{Id: "new"},{Id: "used"},{Id:"failed"}], valueField: "Id", textField: "Id", width:"150px", align:"center"}, //hidden
-  { name: "RequestType", type: "text", width:"150px", align:"center"},  //hidden
-  { name: "Description", type: "text", width:"150px", align:"center", validate: "required"},
-  { name: "UserID", type: "text", width:"150px", align:"center", validate: "required"},
-  { name: "Env", type: "text", width:"80px", align:"center"},      //hidden
-  { name: "ClientID", type: "text", width:"120px", align:"center"}, //hidden
-  { name: "Gender", type: "text", width:"100px", align:"center", validate: "required", insertTemplate: setDefaultInsert("F")},
-  { name: "BirthDate", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("01/22/1970")},
-  { name: "NationalIdType", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("PR")},
-  { name: "CountryCode", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("CAN")},
-  { name: "AddressLine1", type: "text", width:"250px", align:"center", validate: "required", insertTemplate: setDefaultInsert("1 University Ave")},
-  { name: "AddressCity", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("Toronto")},
-  { name: "AddressState", type: "text", width:"120px", align:"center", validate: "required", insertTemplate: setDefaultInsert("ON")},
-  { name: "AddressPostalCode", type: "text", width:"170px", align:"center", validate: "required", insertTemplate: setDefaultInsert("M5J 2P1")},
-  { name: "Format", type: "text", width:"120px", align:"center", validate: "required", insertTemplate: setDefaultInsert("English")},
-  { name: "PhoneType", type: "text", width:"120px", align:"center", validate: "required", insertTemplate: setDefaultInsert("HOME")},
-  { name: "PhoneNumber", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("413-164-369")},
-  { name: "EmpRecordType", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("1")},
-  { name: "DepartmentCode", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("0000802828")},
-  { name: "FulltimePartTime", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("P")},
-  { name: "HireDate", type: "text", width:"130px", align:"center", validate: "required", insertTemplate: setDefaultInsert("12/14/2014")},
-  { name: "JobCode", type: "text", width:"100px", align:"center", validate: "required", insertTemplate: setDefaultInsert("Other")},
-  { name: "EmpClass", type: "text", width:"100px", align:"center", validate: "required", insertTemplate: setDefaultInsert("65")},
-  { name: "UnionCode", type: "text", width:"100px", align:"center", validate: "required", insertTemplate: setDefaultInsert("O02")},
-  { name: "RateCode", type: "text", width:"100px", align:"center", validate: "required", insertTemplate: setDefaultInsert("NAANNL")},
-  { name: "BenefitSystem", type: "text", width:"130px", align:"center", validate: "required", insertTemplate: setDefaultInsert("BN")},
-  { name: "TypeCompRate", type: "text", width:"130px", align:"center", validate: "required", insertTemplate: setDefaultInsert("75000")},
-  { name: "TypeDepartmentId", type: "text", width:"200px", align:"center", validate: "required", insertTemplate: setDefaultInsert("0000802828")},
-  { name: "BenefitProgramName", type: "text", width:"200px", align:"center", validate: "required", insertTemplate: setDefaultInsert("OMR")},
-  { name: "EnrolmentDate", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("12/14/2014")},
-  { name: "NotificationType", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("General")},
-  { name: "PensionPlanType", type: "text", width:"150px", align:"center", validate: "required", insertTemplate: setDefaultInsert("80")}
+  { name: "ClientID", type: "text", width:"80px", align:"center"}, //hidden
+  { name: "Status", type: "select", items: [{Id: ""},{Id: "submitted"},{Id: "new"},{Id: "used"},{Id:"failed"}], valueField: "Id", textField: "Id", width:"50px", align:"center"}, //hidden
+  { name: "UserID", type: "text", width:"80px", align:"center", validate: "required"},
+  { name: "Description", type: "text", width:"100px", align:"center", validate: "required"},
+  { name: "ID", type: "text", width: "60px", align:"center"}, //hidden
+  { name: "RequestType", type: "text", width:"50px", align:"center"},  //hidden
+  { name: "Env", type: "text", width:"30px", align:"center"},      //hidden
+  { name: "SubmissionDate", type: "text", width:"50px", align:"center"}, //hidden
+  { name: "Gender", type: "text", width:"30px", align:"center", validate: "required", insertTemplate: setDefaultInsert("F")},
+  { name: "BirthDate", type: "text", width:"50px", align:"center", validate: "required", insertTemplate: setDefaultInsert("01/22/1970")},
+  { name: "NationalIdType", type: "text", width:"50px", align:"center", validate: "required", insertTemplate: setDefaultInsert("PR")},
+  { name: "CountryCode", type: "text", width:"50px", align:"center", validate: "required", insertTemplate: setDefaultInsert("CAN")},
+  { name: "AddressLine1", type: "text", width:"100px", align:"center", validate: "required", insertTemplate: setDefaultInsert("1 University Ave")},
+  { name: "AddressCity", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("Toronto")},
+  { name: "AddressState", type: "text", width:"50px", align:"center", validate: "required", insertTemplate: setDefaultInsert("ON")},
+  { name: "AddressPostalCode", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("M5J 2P1")},
+  { name: "Format", type: "text", width:"50px", align:"center", validate: "required", insertTemplate: setDefaultInsert("English")},
+  { name: "PhoneType", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("HOME")},
+  { name: "PhoneNumber", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("413-164-369")},
+  { name: "EmpRecordType", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("1")},
+  { name: "DepartmentCode", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("0000802828")},
+  { name: "FulltimePartTime", type: "text", width:"60px", align:"center", validate: "required", insertTemplate: setDefaultInsert("P")},
+  { name: "HireDate", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("12/14/2014")},
+  { name: "JobCode", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("Other")},
+  { name: "EmpClass", type: "text", width:"50px", align:"center", validate: "required", insertTemplate: setDefaultInsert("65")},
+  { name: "UnionCode", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("O02")},
+  { name: "RateCode", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("NAANNL")},
+  { name: "BenefitSystem", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("BN")},
+  { name: "TypeCompRate", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("75000")},
+  { name: "TypeDepartmentId", type: "text", width:"100px", align:"center", validate: "required", insertTemplate: setDefaultInsert("0000802828")},
+  { name: "BenefitProgramName", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("OMR")},
+  { name: "EnrolmentDate", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("12/14/2014")},
+  { name: "NotificationType", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("General")},
+  { name: "PensionPlanType", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("80")},
+  { name: "MemberClass", type: "text", width:"70px", align:"center", validate: "required", insertTemplate: setDefaultInsert("NRA65")}
+
 ];
+
+function getDateNow() {
+  var today = new Date();
+  var dd = today.getDate();
+
+  var mm = today.getMonth()+1;
+  var yyyy = today.getFullYear();
+  if(dd<10)
+  {
+      dd='0'+dd;
+  }
+
+  if(mm<10)
+  {
+      mm='0'+mm;
+  }
+  return mm+'/'+dd+'/'+yyyy;
+}
 
 function setDefaultInsert (value) {
   return function () {
@@ -101,6 +122,7 @@ $(document).ready(function() {
           item.Status = "submitted";
           item.Env = $("#envData", window.parent.document).val();
           item.ClientID = "";
+          item.SubmissionDate = getDateNow();
           item.ID = guid();
           for (var row in currDb) {
             while (currDb[row].ID == item.ID) {
@@ -124,6 +146,7 @@ $(document).ready(function() {
     $("#jsGrid").jsGrid("fieldOption", "Env", "visible", false);
     $("#jsGrid").jsGrid("fieldOption", "ClientID", "visible", false);
     $("#jsGrid").jsGrid("fieldOption", "ID", "visible", false);
+    $("#jsGrid").jsGrid("fieldOption", "SubmissionDate", "visible", false);
   });
 
   $('body').on('click', '.save', function() {
@@ -293,6 +316,7 @@ $(document).ready(function() {
     $("#jsGrid").jsGrid("fieldOption", "Env", "visible", true);
     $("#jsGrid").jsGrid("fieldOption", "ClientID", "visible", true);
     $("#jsGrid").jsGrid("fieldOption", "ID", "visible", true);
+    $("#jsGrid").jsGrid("fieldOption", "SubmissionDate", "visible", true);
 
   });
 
